0c0ce8640b8c000e86722baabfe47f81641f45f2
rendie <121609433+rendie-com@users.noreply.github.com>
2025-11-24 16:34:31 +0800

    Update 1day1.js

--- node/1day1.js
+++ node/1day1.js
@@ -52,7 +52,7 @@ import path from 'path';
   await page.goto(url);
   /////////////////////////////////// 
   let count = 0;
-  let total =  (60*30) / 10//最多运行1小时
+  let total =  (60*1) / 10//最多运行1小时
   let Enable = true;
   while (Enable) {
     count++
